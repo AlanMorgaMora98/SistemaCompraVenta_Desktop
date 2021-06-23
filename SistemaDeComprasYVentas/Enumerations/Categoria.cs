012c72583db0963b27bd18d3c81d9ecadff087f2
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-06-23 17:14:34 -0500

    Creación de enumeración Categoria utilizada por la entidad Publicacion.

---
+++ SistemaDeComprasYVentas/Enumerations/Categoria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeComprasYVentas.Enumerations
+{
+	public enum Categoria
+	{
+		tecnologia,
+		cocina,
+		hogar,
+		juguetesYBebes,
+		moda,
+		deportes,
+		herramientas,
+		industriasYOficina,
+		accesoriosParaVehiculos,
+		saludEquipoMedico,
+		bellezaCuidadoPersonal,
+		calzado,
+		productosSustentables,
+		mascotas
+	}
+}
