15b062dff4dfc29283b7c51f3dd97fece973941b
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-06-23 17:10:21 -0500

    Implementatación de componentes principales de la pantalla BuscarPublicación. Creación de entidad Publicación.

---
+++ SistemaDeComprasYVentas/Models/Publicacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeComprasYVentas.Models
+{
+	public class Publicacion
+	{
+		private int clave_publicacion;
+		private string nombre;
+		private string descripcion;
+		//private Categoria categoria;
+		private double precio;
+		private int cantidad_disponible;
+		private double calificacion_general;
+		private string unidad_medida;
+		private int numero_ventas;
+		private string imagen;
+	}
+}
