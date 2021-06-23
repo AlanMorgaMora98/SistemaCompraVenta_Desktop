bcb53b57a7fc7f318190742ea1a56df571f4dc11
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-06-23 18:00:10 -0500

    Implementación temprana de listas que serán utilizadas para llenar la lista de publicaciones que existen en el sistema.

--- SistemaDeComprasYVentas/ViewModels/BuscarPublicacionesViewModel.cs
+++ SistemaDeComprasYVentas/ViewModels/BuscarPublicacionesViewModel.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SistemaDeComprasYVentas.Models;
+using SistemaDeComprasYVentas.Enumerations;
 
 namespace SistemaDeComprasYVentas.ViewModels
 {
 	public class BuscarPublicacionesViewModel : ViewModelBase
 	{
+		public List< Publicacion > PublicacionesTotales { get; set; }
+		public List< Publicacion > PublicacionesFiltradas { get; set; }
+		public List< Categoria > CriteriosFiltracion { get; set; }
 	}
 }
