92aed23ed351573e09d165cf382f6d1f4218ffff
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-06-24 16:55:10 -0500

    Implementación de primera versión de LoginSession.

--- SistemaDeComprasYVentas/Session/LoginSession.cs
+++ SistemaDeComprasYVentas/Session/LoginSession.cs
@@ -3,11 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SistemaDeComprasYVentas.Models;
 
 namespace SistemaDeComprasYVentas.Session
 {
-	public class LoginSession
+	class LoginSession
 	{
 		private static LoginSession loginInstance;
+		public Usuario Usuario { get; set; }
+		public int ClaveUsuario { get; set; }
+		public string AccessToken { get; set; }
+		public bool IsLoggedIn
+		{
+			get { return ClaveUsuario > -1; }
+		}
+
+
+		private LoginSession()
+		{
+			Usuario = null;
+			ClaveUsuario = -1;
+			AccessToken = "";
+		}
+
+		public static LoginSession GetInstance()
+		{
+			if( loginInstance == null )
+			{
+				loginInstance = new LoginSession();
+			}
+			return loginInstance;
+		}
+
+		public void Logout()
+		{
+			if( IsLoggedIn )
+			{
+				ClaveUsuario = -1;
+				AccessToken = "";
+				Usuario = null;
+			}
+		}
+
+		public void Login( int claveIn, string tokenIn )
+		{
+			if( !IsLoggedIn && ClaveUsuario == -1 )
+			{
+				ClaveUsuario = claveIn;
+				AccessToken = tokenIn;
+			}
+		}
 	}
 }
