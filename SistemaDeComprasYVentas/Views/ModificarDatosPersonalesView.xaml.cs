ab54ea3a5b94189db85f811808808d00ae42134a
Alan Adair Morgado Morales <AlanMorgadoMORALES1998@outlook.com>
2021-06-24 05:21:34 -0500

    Se agrego la ventana para modificar los datos personales del usuario

---
+++ SistemaDeComprasYVentas/Views/ModificarDatosPersonalesView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SistemaDeComprasYVentas.Views
+{
+    /// <summary>
+    /// Lógica de interacción para ModificarDatosPersonalesView.xaml
+    /// </summary>
+    public partial class ModificarDatosPersonalesView : UserControl
+    {
+        public ModificarDatosPersonalesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
