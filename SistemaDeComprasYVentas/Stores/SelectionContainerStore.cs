7c3d2c51d5e55ad761cec6b2613d3a9c602fabdc
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-06-25 22:27:54 -0500

    Creación de Vista y su modelo de vista VisualizarPantallaComprador. Creación de elementos gráficos de interfaz de usuario para la vista VisualizarPantallaComprador. Creación e implementación básica de clase SelectionContainerStore.

---
+++ SistemaDeComprasYVentas/Stores/SelectionContainerStore.cs
@@ -0,0 +1,29 @@
+﻿using SistemaDeComprasYVentas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeComprasYVentas.Stores
+{
+	public class SelectionContainerStore
+	{
+		private static SelectionContainerStore selectionContainerStore;
+		public Publicacion Publicacion { get; set; }
+
+		private SelectionContainerStore()
+		{
+			Publicacion = null;
+		}
+
+		public static SelectionContainerStore GetInstance()
+		{
+			if( selectionContainerStore == null )
+			{
+				selectionContainerStore = new SelectionContainerStore();
+			}
+			return selectionContainerStore;
+		}
+	}
+}
