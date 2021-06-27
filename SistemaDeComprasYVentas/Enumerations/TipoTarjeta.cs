724be65d12084d6ad8e7a0c985d39aac488d8416
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-06-27 17:38:32 -0500

    Creación de modelo Tarjeta y su enumeración TipoTarjeta.

---
+++ SistemaDeComprasYVentas/Enumerations/TipoTarjeta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeComprasYVentas.Enumerations
+{
+	public enum TipoTarjeta
+	{
+		debito,
+		credito
+	}
+}
