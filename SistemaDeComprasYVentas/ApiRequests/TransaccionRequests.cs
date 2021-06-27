a9912ac4b6501acf2ddd323be9a2b89741f5a47b
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-06-27 18:46:47 -0500

    Creación e implementación de método para recuparar la clave del usuario de una publicación.

---
+++ SistemaDeComprasYVentas/ApiRequests/TransaccionRequests.cs
@@ -0,0 +1,39 @@
+﻿using SistemaDeComprasYVentas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeComprasYVentas.ApiRequests
+{
+	public class TransaccionRequests
+	{
+		private string publicacionClaveURL = "http://192.168.1.68:5000/publicaciones/";
+
+		public TransaccionRequests()
+		{
+			ApiHelper.InitializeClient();
+		}
+
+		public async Task< UsuarioPublicacion > RecuperarClaveUsuario( int clavePublicacion, string accessToken )
+		{
+			string requestURL = publicacionClaveURL + clavePublicacion;
+			ApiHelper.ApiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue( "Bearer", accessToken );
+			using( HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync( requestURL ) )
+			{
+				if( response.IsSuccessStatusCode )
+				{
+					UsuarioPublicacion usuarioPublicacion = await response.Content.ReadAsAsync< UsuarioPublicacion >();
+					return usuarioPublicacion;
+				}
+				else
+				{
+					return null;
+				}
+			}
+		}
+	}
+}
--- SistemaDeComprasYVentas/Commands/RealizarPedidoCommand.cs
+++ SistemaDeComprasYVentas/Commands/RealizarPedidoCommand.cs
@@ -9,6 +9,7 @@ namespace SistemaDeComprasYVentas.Commands
 	public class RealizarPedidoCommand : CommandBase
 	{
 
+
 		public override void Execute( object parameter )
 		{
 			throw new NotImplementedException();
