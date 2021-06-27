8259cce7d90e4b41c84506f3e7e56255f8fba412
Alan Adair Morgado Morales <AlanMorgadoMORALES1998@outlook.com>
2021-06-26 22:28:50 -0500

    agregue un nuevo enumeration para los tipos de medida del producto de la publicacion

---
+++ SistemaDeComprasYVentas/Enumerations/UnidadMedida.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeComprasYVentas.Enumerations
+{
+    public enum UnidadMedida
+    {
+        mililitros,
+        kilogramos,
+        onzas,
+        centrimetros,
+        metros,
+        gramos,
+        litros
+    }
+}
