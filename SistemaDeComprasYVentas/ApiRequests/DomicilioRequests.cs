3982a10e5489b31734b023d3d2dce7225933cf1d
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-06-27 17:10:26 -0500

    Creación de Clase DomiciliosRequests para poder mandar solicitudes al api relaiconados con domicilios.

---
+++ SistemaDeComprasYVentas/ApiRequests/DomicilioRequests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeComprasYVentas.ApiRequests
+{
+	public class DomicilioRequests
+	{
+		private string usuarioGeneralURL = "http://192.168.1.68:5000/usuarios";
+
+		public DomicilioRequests()
+		{
+			ApiHelper.InitializeClient();
+		}
+
+		public Task< Domicilio > RecuperarDomiciliosUsuario( int claveUsuario, string accessToken )
+		{
+
+		}
+	}
+}
--- SistemaDeComprasYVentas/ViewModels/RealizarPedidoViewModel.cs
+++ SistemaDeComprasYVentas/ViewModels/RealizarPedidoViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SistemaDeComprasYVentas.Models;
+using SistemaDeComprasYVentas.Stores;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,43 @@ namespace SistemaDeComprasYVentas.ViewModels
 {
 	public class RealizarPedidoViewModel : ViewModelBase
 	{
+		private string productosTotales;
+		private string subtotal;
+
+		public string ProductosTotales 
+		{
+			get { return productosTotales; }
+			set
+			{
+				productosTotales = value;
+				OnPropertyChanged( nameof( ProductosTotales ) );
+			}
+		}
+
+		public string Subtotal 
+		{ 
+			get { return subtotal; } 
+			set
+			{
+				subtotal = value;
+				OnPropertyChanged( nameof( Subtotal ) );
+			}
+		}
+
+		public RealizarPedidoViewModel()
+		{
+			ProductosTotales = SelectionContainerStore.GetInstance().PublicacionesCarrito.Count.ToString();
+			Subtotal = CalculateSubtotal();
+		}
+
+		private string CalculateSubtotal()
+		{
+			double subtotal = 0.0;
+			foreach( Publicacion publicacion in SelectionContainerStore.GetInstance().PublicacionesCarrito )
+			{
+				subtotal += publicacion.precio;
+			}
+			return subtotal.ToString();
+		}
 	}
 }
