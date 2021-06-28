7457bb8d2feeacb101012d05ad485fea9a167175
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-06-27 22:01:29 -0500

    Implentación de Funcionalidad para visualizar historial de compras.

--- SistemaDeComprasYVentas/ViewModels/HistorialComprasViewModel.cs
+++ SistemaDeComprasYVentas/ViewModels/HistorialComprasViewModel.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using SistemaDeComprasYVentas.ApiRequests;
+using SistemaDeComprasYVentas.Models;
+using SistemaDeComprasYVentas.Session;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +12,34 @@ namespace SistemaDeComprasYVentas.ViewModels
 {
 	public class HistorialComprasViewModel : ViewModelBase
 	{
+		private TransaccionRequests requests;
+		private ObservableCollection< Transaccion > transaccionesUsuario;
+		public ObservableCollection< Transaccion > TransaccionesUsuario
+		{
+			get { return transaccionesUsuario; }
+			set
+			{
+				transaccionesUsuario = value;
+				OnPropertyChanged( nameof( TransaccionesUsuario ) );
+			}
+		}
+
+		public HistorialComprasViewModel()
+		{
+			requests = new TransaccionRequests();
+			RecuperarTransacciones();
+		}
+
+		private void RecuperarTransacciones()
+		{
+			requests.RecuperarTransaccionesUsuario( LoginSession.GetInstance().ClaveUsuario,
+													LoginSession.GetInstance().AccessToken ).ContinueWith( Task =>
+				{
+					if (Task.Exception == null)
+					{
+						TransaccionesUsuario = Task.Result;
+					}
+				} );
+		}
 	}
 }
