4700895a8e3477fc1b20021fb14122ebbeca7e33
Alan Adair Morgado Morales <AlanMorgadoMORALES1998@outlook.com>
2021-06-28 11:54:49 -0500

    nuevos elementos de la vista de domicilios

--- SistemaDeComprasYVentas/Views/DomiciliosView.xaml.cs
+++ SistemaDeComprasYVentas/Views/DomiciliosView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaDeComprasYVentas.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,19 @@ namespace SistemaDeComprasYVentas.Views
 		public DomiciliosView()
 		{
 			InitializeComponent();
+		}
+
+		public void SelectionChanged(object sender, RoutedEventArgs e)
+		{
+			if (((ListBox)sender).SelectedIndex > -1)
+			{
+				((dynamic)DataContext).ItemSeleccionado = ( Domicilio )((ListBox)sender).SelectedItem;
+			}
+		}
+
+		public void EliminarDomicilio(object sender, RoutedEventArgs e)
+		{
+			((dynamic)DataContext).EliminarPublicacionDeCarrito();
 		}
 	}
 }
