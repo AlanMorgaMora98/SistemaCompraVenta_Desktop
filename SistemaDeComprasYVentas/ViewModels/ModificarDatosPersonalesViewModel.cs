c086a9d3284e80d7cfb4e87fadb50d11e218f982
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-06-29 13:45:03 -0500

    Agregue validación y sanitización de cadenas de usuario para la funcionalidad de ModificarDatosPersonales.

--- SistemaDeComprasYVentas/ViewModels/ModificarDatosPersonalesViewModel.cs
+++ SistemaDeComprasYVentas/ViewModels/ModificarDatosPersonalesViewModel.cs
@@ -6,11 +6,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using SistemaDeComprasYVentas.Commands;
+using SistemaDeComprasYVentas.Utilities;
 
 namespace SistemaDeComprasYVentas.ViewModels
 {
     public class ModificarDatosPersonalesViewModel : ViewModelBase
     {
+		private StringValidator validator;
+		private OutputMessages messages;
 		public ICommand ModificarDatosCommand { get; }
 
 		private string nombres;
@@ -20,12 +23,14 @@ namespace SistemaDeComprasYVentas.ViewModels
 		private string telefono;
 		private SecureString contrasena;
 		private SecureString confirmarContrasena;
+		private string errorText;
 		public string Nombres
 		{
 			get { return nombres; }
 			set
 			{
 				nombres = value;
+				AreNombresValid( nombres );
 				( ( ModificarDatosUsuarioCommand )ModificarDatosCommand ).nombres = nombres;
 			}
 		}
@@ -35,6 +40,7 @@ namespace SistemaDeComprasYVentas.ViewModels
 			set
 			{
 				apellidos = value;
+				AreApellidosValid( apellidos );
 				( ( ModificarDatosUsuarioCommand )ModificarDatosCommand ).apellidos = apellidos;
 			}
 		}
@@ -44,6 +50,7 @@ namespace SistemaDeComprasYVentas.ViewModels
 			set
 			{
 				correo = value;
+				IsCorreoValid( correo );
 				( ( ModificarDatosUsuarioCommand )ModificarDatosCommand ).correo = correo;
 			}
 		}
@@ -53,6 +60,7 @@ namespace SistemaDeComprasYVentas.ViewModels
 			set
 			{
 				usuario = value;
+				IsUsuarioValid( usuario );
 				( ( ModificarDatosUsuarioCommand )ModificarDatosCommand ).usuario = usuario;
 			}
 		}
@@ -62,6 +70,7 @@ namespace SistemaDeComprasYVentas.ViewModels
 			set
 			{
 				telefono = value;
+				IsTelefonoValid( telefono );
 				( ( ModificarDatosUsuarioCommand )ModificarDatosCommand ).telefono = telefono;
 			}
 		}
@@ -71,6 +80,7 @@ namespace SistemaDeComprasYVentas.ViewModels
 			set
 			{
 				contrasena = value;
+				IsContrasenaValid( ( ( ModificarDatosUsuarioCommand )ModificarDatosCommand ).convertToUNSecureString( contrasena ) );
 				( ( ModificarDatosUsuarioCommand )ModificarDatosCommand ).contrasena = contrasena;
 			}
 		}
@@ -80,13 +90,95 @@ namespace SistemaDeComprasYVentas.ViewModels
 			set
 			{
 				confirmarContrasena = value;
+				DoContrasenasMatch( ( ( ModificarDatosUsuarioCommand )ModificarDatosCommand ).convertToUNSecureString( Contrasena ),
+								    ( ( ModificarDatosUsuarioCommand )ModificarDatosCommand ).convertToUNSecureString( confirmarContrasena ) );
 				( ( ModificarDatosUsuarioCommand )ModificarDatosCommand ).confirmarContrasena = confirmarContrasena;
+			}
+		}
+		public string ErrorText
+		{
+			get { return errorText; }
+			set
+			{
+				errorText = value;
+				OnPropertyChanged( nameof( ErrorText ) );
 			}
 		}
 
 		public ModificarDatosPersonalesViewModel()
 		{
+			validator = new StringValidator();
+			messages = new OutputMessages();
 			ModificarDatosCommand = new ModificarDatosUsuarioCommand();
+		}
+
+		private void AreNombresValid( string nombres )
+		{
+			ErrorText = "";
+			if( !string.IsNullOrEmpty( nombres ) && !validator.AreNamesValid( nombres ) )
+			{
+				SetErrorMessage( messages.NombresInvalidosMessage() );
+			}
+		}
+
+		private void AreApellidosValid( string apellidos )
+		{
+			ErrorText = "";
+			if( !string.IsNullOrEmpty( apellidos ) && !validator.AreLastNamesValid( apellidos ) )
+			{
+				SetErrorMessage( messages.ApellidosInvalidosMessage() );
+			}
+		}
+
+		private void IsCorreoValid( string correo )
+		{
+			ErrorText = "";
+			if( !string.IsNullOrEmpty( correo ) && !validator.IsEmailValid( correo ) )
+			{
+				SetErrorMessage( messages.CorreoInvalidoMessage() );
+			}
+		}
+
+		private void IsTelefonoValid( string telefono )
+		{
+			ErrorText = "";
+			if( !string.IsNullOrEmpty( telefono ) && !validator.IsTelephoneValid( telefono ) )
+			{
+				SetErrorMessage( messages.TelefonoInvalidoMessage() );
+			}
+		}
+
+		private void IsUsuarioValid( string usuario )
+		{
+			ErrorText = "";
+			if( !string.IsNullOrEmpty( usuario ) && !validator.IsUsernameValid( usuario ) )
+			{
+				SetErrorMessage( messages.UsuarioInvalidMessage() );
+			}
+		}
+
+		private void IsContrasenaValid( string contrasena )
+		{
+			ErrorText = "";
+			if( !string.IsNullOrEmpty( contrasena ) && !validator.IsPasswordValid( contrasena ) )
+			{
+				SetErrorMessage( messages.ContrasenaInvalidMessage() );
+			}
+		}
+
+		private void DoContrasenasMatch( string contrasena, string confirmarContrasena )
+		{
+			ErrorText = "";
+			if( !string.IsNullOrEmpty( contrasena ) && !string.IsNullOrEmpty( confirmarContrasena ) &&
+				!validator.DoPasswordsMatch( contrasena, confirmarContrasena ) )
+			{
+				SetErrorMessage( messages.ContrasenasNoCoincidenMessage() );
+			}
+		}
+
+		private void SetErrorMessage( string errorText )
+		{
+			ErrorText = errorText;
 		}
 	}
 }
