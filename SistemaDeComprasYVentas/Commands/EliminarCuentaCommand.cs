59c57033b96461530ff797ee7bc248c37b7a00e2
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-06-29 16:31:14 -0500

    Agregué funcionalidad a EliminarCuentaCommand para que haga un logout y cambie de vista al eliminar la cuenta del usuario actual.

--- SistemaDeComprasYVentas/Commands/EliminarCuentaCommand.cs
+++ SistemaDeComprasYVentas/Commands/EliminarCuentaCommand.cs
@@ -1,29 +1,36 @@
 ﻿using SistemaDeComprasYVentas.ApiRequests;
 using SistemaDeComprasYVentas.Session;
+using SistemaDeComprasYVentas.Utilities;
+using SistemaDeComprasYVentas.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SistemaDeComprasYVentas.Commands
 {
 	public class EliminarCuentaCommand : CommandBase
 	{
 		private UsuarioRequests requests;
+		private ICommand NavigateBuscarProductosCommand { get; }
 
 		public EliminarCuentaCommand()
 		{
 			requests = new UsuarioRequests();
+			NavigateBuscarProductosCommand = new NavigateCommand< BuscarPublicacionesViewModel >( 
+												 NavigationServiceCreator.GetInstance().CreateBuscarNavigationService() );
 		}
 
 		public override void Execute( object parameter )
 		{
 			requests.DeleteUsuario( LoginSession.GetInstance().ClaveUsuario, LoginSession.GetInstance().AccessToken ).ContinueWith( Task => 
 			{
-				if( Task.Exception != null )
+				if( Task.Exception == null )
 				{
-
+					LoginSession.GetInstance().Logout();
+					NavigateBuscarProductosCommand.Execute( this );
 				}
 			} );
 		}
